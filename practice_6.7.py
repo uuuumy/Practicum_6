5941602109f8c405c5215a7ea0f82ea575b7f9e3
Ayla-Solo <o.soloveva2@g.nsu.ru>
2025-12-02 00:11:24 +0700

    practice_6.7.py

--- practice_6.7.py
+++ practice_6.7.py
@@ -1,5 +1,12 @@
-n = input()
-if n[-1] == '0' or n[-1] == '5' or n[-1] == '7' or (n[-1] == '2' and n != '2') or int(n) % 7 == 0:
-    print('да')
+k = int(input())
+
+can = False
+for i in range(k // 5 + 1):
+    for j in range(k // 7 + 1):
+        if (5*i + 7*j) == k:
+            can = True
+
+if can:
+    print("Да")
 else:
-    print('нет')
+    print("Нет")
