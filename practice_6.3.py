add7d6bbfce5284cb4c721eda9af5ae560da6a4a
Ayla-Solo <o.soloveva2@g.nsu.ru>
2025-12-02 10:40:20 +0700

    practice_6.3.py

--- practice_6.3.py
+++ practice_6.3.py
@@ -1,6 +1,7 @@
-n, m = map(int,input().split("x"))
-k = input()
-if int(n) + int(m) == int(k):
-    print('успешно')
+n, m = map(int, input().split("x"))
+k = int(input())
+
+if k < n * m and (k % n == 0 or k % m == 0):
+    print("Успешно")
 else:
-    print('неосуществимо')
+    print("Неосуществимо")
